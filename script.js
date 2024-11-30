261e7b8ed95164b53d019cbeab03e1ae526f6dde
Eslam Omar <eslam.O.elkhabery@gmail.com>
2024-11-30 23:55:59 +0200

    add the map

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude, longitude, accuracy } = position.coords;
+      console.log(
+        `Latitude: ${latitude}, Longitude: ${longitude}, Accuracy: ${accuracy} meters`
+      );
+
+      const map = L.map('map').setView([latitude, longitude], 17);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      const marker = L.marker([latitude, longitude])
+        .bindPopup('<b>≈ Current location</b>')
+        .addTo(map);
+    },
+    error => {
+      if (error.code === 1) {
+        alert('We want your permission');
+      }
+      console.error(`Error (${error.code}): ${error.message}`);
+    },
+    {
+      enableHighAccuracy: true, // Requests more accurate results
+      timeout: 100000, // Timeout in milliseconds
+      maximumAge: 0, // No cached results
+    }
+  );
+} else {
+  console.error('Geolocation is not supported by this browser.');
+}
